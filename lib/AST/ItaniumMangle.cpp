12b1c25b3957bf3d5a598f0f3f7d7d54b546abc6
Matus Chochlik <chochlik@gmail.com>
2016-09-08 10:00:54 +0200

    [Reflection] Implementation of reflection (P0194Rx)
    - Added the LangOpt::Reflection option.
    - Added the __metaobject_id type representing compile-time metaobject.
    - Added the __reflexpr operator returning a constexpr __metaobject_id
      value.
    - Added the __unrefltype operator implementing reverse reflection of
      types from metaobject ids.
    - Added the __metaobject_op_* metaobject operation compiler built-ins.
    - Created the `example/reflection` directory.
    - Added debug helper scripts to the example directory.
    - Created the `reflection` directory for header files.
    - Added the `reflection/reflexpr` header file implementing the
      metaobject types, the metaobject concepts and the metaobject operations.
    - Added the __unpack_metaobject_seq built-in template
      - Implemented by: Ricardo Fabiano de Andrade
    - Added several tests
      - Implemented by: Ricardo Fabiano de Andrade, Matus Chochlik
    - Added reflects_same
      - Implemented by Mikhail Strelnikov

--- lib/AST/ItaniumMangle.cpp
+++ lib/AST/ItaniumMangle.cpp
@@ -1901,6 +1901,7 @@ bool CXXNameMangler::mangleUnresolvedTypeOrSimpleId(QualType Ty,
   case Type::TypeOfExpr:
   case Type::TypeOf:
   case Type::Decltype:
+  case Type::Unrefltype:
   case Type::TemplateTypeParm:
   case Type::UnaryTransform:
   case Type::SubstTemplateTypeParm:
@@ -2410,6 +2411,9 @@ void CXXNameMangler::mangleType(const BuiltinType *T) {
   case BuiltinType::ULong:
     Out << 'm';
     break;
+  case BuiltinType::MetaobjectId:
+    Out << "mo";
+    break;
   case BuiltinType::ULongLong:
     Out << 'y';
     break;
@@ -2890,6 +2894,7 @@ static StringRef mangleAArch64VectorBase(const BuiltinType *EltType) {
     return "Float32";
   case BuiltinType::Double:
     return "Float64";
+  case BuiltinType::MetaobjectId:
   default:
     llvm_unreachable("Unexpected vector element base type");
   }
@@ -2921,6 +2926,7 @@ void CXXNameMangler::mangleAArch64NeonVectorType(const VectorType *T) {
     case BuiltinType::ULongLong:
       EltName = "Poly64";
       break;
+    case BuiltinType::MetaobjectId:
     default:
       llvm_unreachable("unexpected Neon polynomial vector element type");
     }
@@ -3121,6 +3127,15 @@ void CXXNameMangler::mangleType(const DecltypeType *T) {
     Out << "Dt";
   else
     Out << "DT";
+  mangleExpression(E);
+  Out << 'E';
+}
+
+void CXXNameMangler::mangleType(const UnrefltypeType *T) {
+  Expr *E = T->getUnderlyingExpr();
+
+  // TODO[reflexpr] is this OK?
+  Out << "UT";
   mangleExpression(E);
   Out << 'E';
 }
@@ -3681,6 +3696,30 @@ recurse:
       Out << 'z';
       mangleExpression(SAE->getArgumentExpr());
     }
+    break;
+  }
+
+  case Expr::ReflexprExprClass: {
+    // TODO[reflexpr] is this OK?
+    Out << "mo";
+    break;
+  }
+
+  case Expr::MetaobjectIdExprClass: {
+    // TODO[reflexpr] is this OK?
+    Out << "ma";
+    break;
+  }
+
+  case Expr::UnaryMetaobjectOpExprClass: {
+    // TODO[reflexpr] is this OK?
+    Out << "mp";
+    break;
+  }
+
+  case Expr::NaryMetaobjectOpExprClass: {
+    // TODO[reflexpr] is this OK?
+    Out << "mp";
     break;
   }
 
--- lib/AST/MicrosoftMangle.cpp
+++ lib/AST/MicrosoftMangle.cpp
@@ -1723,6 +1723,9 @@ void MicrosoftCXXNameMangler::mangleType(const BuiltinType *T, Qualifiers,
   case BuiltinType::ULong:
     Out << 'K';
     break;
+  case BuiltinType::MetaobjectId:
+    Out << "MO";
+    break;
   case BuiltinType::Float:
     Out << 'M';
     break;
@@ -2460,6 +2463,15 @@ void MicrosoftCXXNameMangler::mangleType(const DecltypeType *T, Qualifiers,
   DiagnosticsEngine &Diags = Context.getDiags();
   unsigned DiagID = Diags.getCustomDiagID(DiagnosticsEngine::Error,
     "cannot mangle this decltype() yet");
+  Diags.Report(Range.getBegin(), DiagID)
+    << Range;
+}
+
+void MicrosoftCXXNameMangler::mangleType(const UnrefltypeType *T, Qualifiers,
+                                         SourceRange Range) {
+  DiagnosticsEngine &Diags = Context.getDiags();
+  unsigned DiagID = Diags.getCustomDiagID(DiagnosticsEngine::Error,
+    "cannot mangle this unrefltype() yet");
   Diags.Report(Range.getBegin(), DiagID)
     << Range;
 }
